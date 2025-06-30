954d40fd45ea9a673fb1f629c7ac33c655380629
Wian Lewies <lewieswian@gmail.com>
2025-06-30 11:38:39 +0200

    Temp push

--- index.js
+++ index.js
@@ -4,15 +4,54 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const PORT = 3000;
+
+const path = require('path');
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/player', (req, res) => {
+  res.sendFile(__dirname + '/playerView.html');
+});
+
+app.get('/spectator', (req, res) => {
+  res.sendFile(__dirname + '/spectatorView.html');
+});
+
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('Client connected:', socket.id);
+
+  // Receive frame from sender
+  socket.on('frame', (data) => {
+    // Broadcast to all viewers except sender
+    socket.broadcast.emit('frame', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id);
+  });
 });
 
 server.listen(3000, () => {
-  console.log('listening on http://localhost:3000/');
+  console.log('Server running at http://localhost:3000');
 });
+
+// app.use(express.static(path.join(__dirname, 'public'))); // serve files from /public
+
+// app.listen(PORT, () => {
+//   const os = require('os');
+//   const interfaces = os.networkInterfaces();
+//   let localIP;
+
+//   for (const iface of Object.values(interfaces)) {
+//     for (const config of iface) {
+//       if (config.family === 'IPv4' && !config.internal) {
+//         localIP = config.address;
+//       }
+//     }
+//   }
+
+//   console.log(`Server running at http://${localIP}:${PORT}`);
+// });
